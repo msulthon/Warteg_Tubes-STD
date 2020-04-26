7527fe37995aaca62d4b874de5ff0868af711d3b
msulthon <muhammadsulthon82@gmail.com>
2020-04-27 04:12:37 +0700

    Update listParrent.h

--- Tubes Files/listParrent.h
+++ Tubes Files/listParrent.h
@@ -34,7 +34,7 @@ void deleteFirstParent(List_Parent &L, address_parent &P);
 void deleteAfterParent(List_Parent &L, address_parent Prec, address_parent &P);
 void deleteLastParent(List_Parent &L, address_parent &P);
 void deleteSpesificParent(List_Parent &L, string X);
-address_child findElmParent(List_Parent &L, string X);
+address_parent findElmParent(List_Parent &L, string X);
 void printInfoParent(List_Parent L);
 
 void inputDataWarteg(List_Parent L,infotypeParent &in);
